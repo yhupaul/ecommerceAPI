596f76141a5928e77010a1027a526ee910eec7db
yhupaul <yhupaul@hotmail.com>
2022-06-29 16:09:41 -0700

    creat mongooseDB account and connected with it

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
- 
+const mongoose = require("mongoose");
+
+mongoose
+.connect(
+  "mongodb+srv://ydan:Lena1207@cluster0.wnuppec.mongodb.net/shop?retryWrites=true&w=majority"
+  ) //this is secet key
+.then(() => console.log("DBConnection Successfull!"))
+.catch((err) => {
+  console.log(err);
+});
 
 app.listen(8000, () => {
   console.log("Backend server is running!");
